ffae2206ca9288582d0d40f1c36122e2b59dd98b
TuNguyen <ituet1119@gmail.com>
2022-01-03 21:04:19 +0700

    add layout default

--- src/main/java/vnu/uet/cinema_manager/controller/HomeController.java
+++ src/main/java/vnu/uet/cinema_manager/controller/HomeController.java
@@ -6,8 +6,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping(value = {"/"})
+    @GetMapping(value = {"/", "/home"})
     public String home(){
-        return "home/index";
+        return "public/home";
     }
+
+    @GetMapping(value = {"/about"})
+    public String about(){
+        return "public/about";
+    }
+
+    @GetMapping(value = {"/joinus"})
+    public String joinus(){
+        return "public/joinus";
+    }
+
+    @GetMapping(value = {"/contact"})
+    public String contact(){
+        return "public/contact";
+    }
+
 }
